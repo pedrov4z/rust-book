815cb2c88b4dd435e80385fa25a6793ab7ddf86e
Pedro Vaz <ppvaz03@gmail.com>
2023-07-09 20:47:40 -0300

    feat(chapter-8): commit exercise 1 exposition

---
+++ 8-common-collections/8-22-1-summary-exercise-1.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("\nGiven a list of integers, use a vector and return the median (when sorted, the value in the middle position) and mode (the value that occurs most often; a hash map will be helpful here) of the list.\n");
+}
+
