961e0a02c36d6a54801ab07b15d198ffcb3fa676
Pedro Vaz <ppvaz03@gmail.com>
2023-07-10 19:59:46 -0300

    feat(chapter-8): solve summary exercise 1

--- 8-common-collections/8-22-1-summary-exercise-1.rs
+++ 8-common-collections/8-22-1-summary-exercise-1.rs
@@ -35,30 +35,38 @@ fn get_modes(integers: &Vec<i8>) {
         return;
     }
     let mode_type = get_mode_type(modes.len());
-    println!("Conjunto {0} cujas modas são {1:#?}, com {2} ocorrências cada.\n", mode_type, modes, max_times);
+    println!("Conjunto {0} cujas modas são {1:?}, com {2} ocorrências cada.\n", mode_type, modes, max_times);
+}
+
+fn get_median(integers: &Vec<i8>) {
+    let is_even = integers.len() % 2 == 0;
+    if is_even {
+        let middle_index = integers.len() / 2;
+        let first_middle = integers[middle_index - 1];
+        let second_middle = integers[middle_index];
+        let median = (first_middle + second_middle) as f32 / 2.0;
+        println!("Median is {median}.\n");
+        return;
+    }
+    let middle_index = (integers.len() - 1) / 2;
+    println!("Median is {0}.\n", integers[middle_index]);
 }
 
 fn main() {
     println!("\nGiven a list of integers, use a vector and return the median (when sorted, the value in the middle position) and mode (the value that occurs most often; a hash map will be helpful here) of the list.\n");
-    let mut integers = vec![1, 2, 0, 5, 3, 5, 4, 4];
-    integers.sort();
-    println!("{:#?}", integers);
-    get_modes(&integers);
-    let mut integers = vec![1, 2, 0, 2, 5, 3, 5, 4, 4];
-    integers.sort();
-    println!("{:#?}", integers);
-    get_modes(&integers);
-    let mut integers = vec![1, 2, 0, 2, 5, 3, 5, 4, 4, 0];
-    integers.sort();
-    println!("{:#?}", integers);
-    get_modes(&integers);
-    let mut integers = vec![1, 2, 0, 2, 5, 3, 5, 4, 4, 0, 0];
-    integers.sort();
-    println!("{:#?}", integers);
-    get_modes(&integers);
-    let mut integers = vec![0, 1, 2, 3, 4, 5];
-    integers.sort();
-    println!("{:#?}", integers);
-    get_modes(&integers);
+    let vector_of_vectors = vec![
+        vec![1, 2, 0, 5, 3, 5, 4, 4],
+        vec![1, 2, 0, 2, 5, 3, 5, 4, 4],
+        vec![1, 2, 0, 2, 5, 3, 5, 4, 4, 0],
+        vec![1, 2, 0, 2, 5, 3, 5, 4, 4, 0, 0],
+        vec![0, 1, 2, 3, 4, 5],
+    ];
+    for mut vector in vector_of_vectors {
+        vector.sort();
+        println!("{:?}\n", vector);
+        get_modes(&vector);
+        get_median(&vector);
+        println!("---------\n");
+    }
 }
 
